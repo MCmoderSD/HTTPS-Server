0a91b6b35df6cf3a6cdfd82f7ccb84bb83ea62c5
mcmodersd <mail@mcmodersd.de>
2025-10-14 12:54:00 +0200

    Release 2.1.0 - improved certificate management by adding support for provided and ACME signed certificates, enhanced error handling, and updated configuration options

--- src/main/java/de/MCmoderSD/server/cert/CertManager.java
+++ src/main/java/de/MCmoderSD/server/cert/CertManager.java
@@ -10,12 +10,15 @@ import java.security.*;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.HexFormat;
 
+@SuppressWarnings("ClassCanBeRecord")
 public class CertManager {
 
     // Constants
     private static final String BC_PROVIDER = "BC";
     private static final String SIGNATURE_ALGORITHM = "SHA512withRSA";
+    private static final String FINGERPRINT_ALGORITHM = "SHA-256";
     private static final String KEYSTORE_TYPE = "PKCS12";
     private static final String SSL_PROTOCOL = "TLS";
 
@@ -35,21 +38,26 @@ public class CertManager {
         // Check Configuration
         if (config == null || config.isNull() || config.isEmpty()) throw new IllegalArgumentException("Certificate configuration cannot be null or empty");
 
-        // Determine Certificate Type
-        boolean provided = config.has("paths") && config.has("provided") && config.get("provided").asBoolean();	// 1. Priority: Provided Certificate
-        boolean acmeSigned = config.has("acmeSigned") && !provided;                                             // 2. Priority: ACME Signed Certificate
-        boolean selfSigned = config.has("selfSigned") && !provided && !acmeSigned;                              // 3. Priority: Self-Signed Certificate
-        if (!provided && !acmeSigned && !selfSigned) throw new IllegalArgumentException("No valid certificate configuration found");
-
         // Initialize SSLContext based on Certificate Type
-        SSLContext ssl = null;
-        //if (provided) ssl = loadCertificate ToDo: Implement loading provided certificate
-        //if (acmeSigned) ssl = useAcmeSigned(config.get("acmeSigned")); ToDo: Implement ACME signed certificate
-        if (selfSigned) ssl = useSelfSigned(config.get("selfSigned"));
+        sslContext = switch (CertificateType.fromConfig(config)) {
+            case PROVIDED -> useProvidedCertificate(config);
+            case ACME_SIGNED -> useAcmeSigned(config.get("acmeSigned"));
+            case SELF_SIGNED -> useSelfSigned(config.get("selfSigned"));
+        };
+    }
 
-        // Final Check
-        if (ssl != null) sslContext = ssl;
-        else throw new IllegalStateException("Failed to initialize SSLContext");
+    private SSLContext useProvidedCertificate(JsonNode config) {
+        return null; // ToDo: Implement loading provided certificate
+    }
+
+    private SSLContext useAcmeSigned(JsonNode config) {
+
+        // Check ACME Config
+        if (config == null || config.isNull() || config.isEmpty()) throw new IllegalArgumentException("Certificate configuration cannot be null or empty");
+
+
+
+        return null; // ToDo: Implement ACME signed certificate
     }
 
     private SSLContext useSelfSigned(JsonNode config) {
@@ -75,46 +83,78 @@ public class CertManager {
         if (certificate == null) throw new IllegalArgumentException("Certificate cannot be null");
 
         // Initialize SSLContext
-        String alias = "selfsigned";
-        var keyManagerFactory = initKeyManagerFactory(alias, privateKey, certificate, keyPassword);
-        var trustManagerFactory = initTrustManagerFactory(alias, certificate);
-        return initSSLContext(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers());
+        return initSSLContext(initKeyManager(privateKey.getPrivate(), certificate, keyPassword), initTrustManager(certificate));
     }
 
-    // 4. Initialize KeyManagerFactory with the KeyPair and Certificate
-    private static KeyManagerFactory initKeyManagerFactory(String alias, KeyPair keyPair, X509Certificate certificate, char[] keyPassword) {
+    // Initialize KeyManager
+    private static KeyManager[] initKeyManager(PrivateKey privateKey, X509Certificate certificate, char[] keyPassword) {
         try {
+
+            // Check inputs
+            if (privateKey == null) throw new IllegalArgumentException("Private key cannot be null");
+            if (certificate == null) throw new IllegalArgumentException("Certificate cannot be null");
+            if (keyPassword == null || keyPassword.length == 0) throw new IllegalArgumentException("Key password cannot be null or empty");
+
+            // Initialize KeyStore
             KeyStore keyStore = KeyStore.getInstance(KEYSTORE_TYPE);
             keyStore.load(null, null);
-            keyStore.setKeyEntry(alias, keyPair.getPrivate(), keyPassword, new Certificate[]{certificate});
+            keyStore.setKeyEntry(
+                    HexFormat.of().formatHex(MessageDigest.getInstance(FINGERPRINT_ALGORITHM).digest(certificate.getEncoded())).toLowerCase(),
+                    privateKey,
+                    keyPassword,
+                    new Certificate[] { certificate }
+            );
+
+            // Initialize KeyManagerFactory
             KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
             keyManagerFactory.init(keyStore, keyPassword);
-            return keyManagerFactory;
+            return keyManagerFactory.getKeyManagers();
+
         } catch (CertificateException | KeyStoreException | IOException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
             throw new RuntimeException("Error initializing KeyManagerFactory", e);
         }
     }
 
-    // 5. Initialize TrustManagerFactory with the Certificate
-    private static TrustManagerFactory initTrustManagerFactory(String alias, X509Certificate certificate) {
+    // Initialize TrustManager
+    private static TrustManager[] initTrustManager(X509Certificate certificate) {
         try {
+
+            // Check input
+            if (certificate == null) throw new IllegalArgumentException("Certificate cannot be null");
+
+            // Initialize TrustStore
             KeyStore trustStore = KeyStore.getInstance(KEYSTORE_TYPE);
             trustStore.load(null, null);
-            trustStore.setCertificateEntry(alias, certificate);
+            trustStore.setCertificateEntry(
+                    HexFormat.of().formatHex(MessageDigest.getInstance(FINGERPRINT_ALGORITHM).digest(certificate.getEncoded())).toLowerCase(),
+                    certificate
+            );
+
+            // Initialize TrustManagerFactory
             TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
             trustManagerFactory.init(trustStore);
-            return trustManagerFactory;
+            return trustManagerFactory.getTrustManagers();
+
         } catch (CertificateException | KeyStoreException | IOException | NoSuchAlgorithmException e) {
             throw new RuntimeException("Error initializing TrustManagerFactory", e);
         }
     }
 
-    // 6. Initialize SSLContext with KeyManager and TrustManager
+    // Initialize SSLContext
     private static SSLContext initSSLContext(KeyManager[] keyManager, TrustManager[] trustManager) {
         try {
+
+            // Check inputs
+            if (keyManager == null || keyManager.length == 0) throw new IllegalArgumentException("KeyManager cannot be null or empty");
+            if (trustManager == null || trustManager.length == 0) throw new IllegalArgumentException("TrustManager cannot be null or empty");
+
+            // Initialize SSLContext
             SSLContext sslContext = SSLContext.getInstance(SSL_PROTOCOL);
             sslContext.init(keyManager, trustManager, RANDOM);
+
+            // Return SSLContext
             return sslContext;
+
         } catch (NoSuchAlgorithmException | KeyManagementException e) {
             throw new RuntimeException("Error initializing SSLContext", e);
         }
@@ -123,5 +163,28 @@ public class CertManager {
     // Getter for SSLContext
     public SSLContext getSSLContext() {
         return sslContext;
+    }
+
+    private enum CertificateType {
+
+        // Types
+        PROVIDED,
+        ACME_SIGNED,
+        SELF_SIGNED;
+
+        // Determine Certificate Type from Configuration
+        public static CertificateType fromConfig(JsonNode config) {
+
+            // Check Configuration
+            if (config == null || config.isNull() || config.isEmpty()) throw new IllegalArgumentException("Certificate configuration cannot be null or empty");
+
+            // Determine Certificate Type
+            if (config.has("paths") && config.has("provided") && config.get("provided").asBoolean()) return PROVIDED;
+            if (config.has("acmeSigned")) return ACME_SIGNED;
+            if (config.has("selfSigned")) return SELF_SIGNED;
+
+            // No valid certificate type found
+            throw new IllegalArgumentException("No valid certificate configuration found");
+        }
     }
 }
